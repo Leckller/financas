8247bf76004fe9ab08df13710c843140e8f30380
ruy <7leckler@gmail.com>
2025-03-30 19:18:15 -0300

    feat: agora verifica se o usuário existe ou a senha esta correta

--- backend/src/test/java/com/ruyCorp/dot/user/AuthDtoExceptionTest.java
+++ backend/src/test/java/com/ruyCorp/dot/user/AuthDtoExceptionTest.java
@@ -1,5 +1,7 @@
 package com.ruyCorp.dot.user;
 
+import com.ruyCorp.dot.service.exception.MessageDto;
+import com.ruyCorp.dot.utils.CreateUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 @DisplayName("User exception tests")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class AuthDtoException {
+public class AuthDtoExceptionTest {
 
   ObjectMapper objectMapper = new ObjectMapper();
 
@@ -25,9 +30,23 @@ public class AuthDtoException {
 
   @Test
   @DisplayName("AUTH-USER-NOT-FOUND: Testa se retorna uma mensagem caso não encontre ou a senha esteja errada")
-  public void authUserNotFoundTest() {
+  public void authUserNotFoundTest() throws Exception {
 
-    
+    String body = objectMapper.writeValueAsString(CreateUser.validUser());
+
+    MvcResult request = mockMvc.perform(
+        MockMvcRequestBuilders
+            .post("/auth/login")
+            .contentType("application/json")
+            .content(body))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andReturn();
+
+    String json = request.getResponse().getContentAsString();
+    MessageDto response = objectMapper.readValue(json, MessageDto.class);
+
+    MessageDto message = new MessageDto("Usuário ou senha inválidos!");
+    assertEquals(message, response);
 
   }
 
