ce25bd553801a761d9cee4363e35bda648fe11e8
ruy <7leckler@gmail.com>
2025-04-07 09:30:33 -0300

    feat: brincando com intents, mas organizei melhor o código

--- android/app/src/main/java/com/example/finance/MainActivity.kt
+++ android/app/src/main/java/com/example/finance/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.finance
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,9 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.finance.data.Adapters.TransactionAdapter
 import com.example.finance.data.Transaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     val recyclerView: RecyclerView by lazy { findViewById(R.id.recyclerView) }
-    val button: Button by lazy { findViewById(R.id.header_profile) }
+    val profileBtn: Button by lazy { findViewById(R.id.header_profile) }
+    val configButton: Button by lazy { findViewById(R.id.header_config) }
 
     val transactionList = listOf<Transaction>(
         Transaction("despesa", 5.28, 1),
@@ -26,11 +28,29 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = TransactionAdapter(transactionList)
 
-        button.setOnClickListener{
-            //Criando o intent para nossa MainActivity2
-            val intent = Intent(this, Profile::class.java)
-            //iniciando a Activity
-            startActivity(intent)
+        profileBtn.setOnClickListener(this)
+        configButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id) {
+
+            R.id.header_config -> {
+                val intent = Intent(this, ConfigActivity::class.java).apply {
+                    putExtra("text", "hahaha, isso é bem legal")
+                }
+                startActivity(intent)
+            }
+
+            R.id.header_profile -> {
+                val intent = Intent(this, ProfileActivity::class.java).apply {
+                    putExtra("text", "Achei interessante.")
+                }
+                startActivity(intent)
+            }
+
         }
 
     }
